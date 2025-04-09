732178122a4eed4771d3bdd487529e79d023c4b4
Alireza Mohebali <a.mohebali@tsetmc.com>
2025-04-09 17:02:30 +0330

    create Hello Pejman

--- hello/views.py
+++ hello/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse ("Hello, World.!")
+
+def pejman(request):
+    return HttpResponse ("Hello pejman!")
